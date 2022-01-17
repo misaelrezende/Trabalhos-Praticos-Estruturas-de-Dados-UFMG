55980d0f31f0ba3ac4578c1b0a270c17e369f046
misaelrezende <38136358+misaelrezende@users.noreply.github.com>
2022-01-17 20:03:33 -0300

    Inicializa TP3

---
+++ Algoritmos 1/TP3/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int F, W;
+    cin>>F>>W;
+
+    for(int i = 0; i < F; i++){
+        for(int j = 0; j < W; j++)
+
+    }
+
+    return 0;
+}
