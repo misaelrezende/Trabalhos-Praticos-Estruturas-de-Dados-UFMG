aba02902f444b98f40647ad8c2e3a3acf208c75b
misaelrezende <38136358+misaelrezende@users.noreply.github.com>
2022-05-28 22:14:14 -0300

    FIX: Forgot to add view.py changes

--- view.py
+++ view.py
@@ -10,14 +10,31 @@ class View:
 
         return int(input("\nDigite a opção: "))
 
-    def getLoginDetail(self):
+    def get_login_detail(self):
         print("\nDigite o numero de login")
         login_number = int(input())
         print("\nDigite a senha de login")
         login_password = int(input())
         return login_number, login_password
+    
+    def start_voting_machine(self):
+        print("### Sistema de votação iniciada ###")
+        print("### Para terminar a votação, digite o número 0 para o titulo de eleitor ###")
 
-    def showLoginDetail(self, result):
+    def finish_voting_machine(self):
+        print("Você quer finalizar a votação?")
+        print("0 - Finalizar votação")
+        print("1 - Continuar votação")
+        print("Digite o login e senha do mesário")
+
+    def voter_is_voting(self, candidate):
+        print("{}".format(candidate))
+        return int(input())
+
+    def voter_is_finished(self):
+        print("FIM")
+
+    def show_login_detail(self, result):
         if result == True:
             print()
             print("Mesário autenticado com sucesso!")
