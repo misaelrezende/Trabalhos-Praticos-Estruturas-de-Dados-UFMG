7f2c168aaccc5c0ff84154b38d04c74b88153600
lucas <lucas.sousa@bancointer.com.br>
2022-05-29 16:27:30 -0300

    alteracoes candidate, clerk

---
+++ clerk.py
@@ -0,0 +1,16 @@
+from users import User
+class Clerk(User):
+    def _initi_(self, startDate, endDate, name, reg_number, zona, secao,listOfCandidate):
+        self.starDate = startDate
+        self.endDate = endDate
+        self.listCandidate = listOfCandidate
+        super().__init__(name, reg_number, zona, secao)
+
+    def get_starDate(self):
+        return self.starDate
+
+    def get_endDate(self):
+        return self.endDate
+
+    def get_listCandidate(self):
+        return self.listCandidate     
