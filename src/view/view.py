ca11ebda48d32b1797ac82d05429d2228d5365ab
misaelrezende <38136358+misaelrezende@users.noreply.github.com>
2022-06-26 22:31:41 -0300

    Add docstring to view module
    Fix some pylint errors

--- src/view/view.py
+++ src/view/view.py
@@ -1,23 +1,50 @@
+"""View module"""
+
 class View:
+    """
+    View class. This class is responsible to interact
+    with the clerk and the voter
+    """
     def start(self):
+        """
+        Show user that system is offline
+        Returns:
+            Invoke self.menu() method
+        """
         print("### Sistema offline ####\n")
         print("Faça login como mesário para iniciar a votação.")
         return self.menu()
 
     def menu(self):
+        """
+        Show the user an option to login or to quit the system
+        Returns:
+            (int): Option chosen by user
+        """
         print("0 - Sair")
         print("1 - Para fazer login como mesario")
         return int(input("\nDigite a opção: "))
 
     def get_login_detail(self):
+        """
+        Get login and password of user
+        Returns:
+            login_number (int): login
+            login_password (int): password
+        """
         print("\nDigite o numero de login")
         login_number = int(input())
         print("\nDigite a senha de login")
         login_password = int(input())
         return login_number, login_password
-    
+
     def show_login_detail(self, result):
-        if result == True:
+        """
+        Show user that he/her is/is not authenticated
+        Arguments:
+            result (bool): result of login attempt
+        """
+        if result is True:
             print()
             print("Mesário autenticado com sucesso!")
         else:
@@ -26,15 +53,30 @@ class View:
             print("Por favor, reinicie a autenticação.")
 
     def start_voting_machine(self):
+        """
+        Show user that the voting process has started
+        """
         print()
         print("### Sistema de votação iniciada ###")
         print("### Para terminar a votação, digite o número 0 para o titulo de eleitor ###")
         print()
 
     def get_voter_registration_number(self):
+        """
+        Get the voter registration number
+        Returns:
+            (int): registration entered by user
+        """
         return int(input("Digite o número do título de eleitor: "))
 
     def finish_voting_machine(self):
+        """
+        Check if clerk wants to finish voting process
+        Returns:
+            option (int): if user input is 0
+            Or else
+            Invoke self.menu() method
+        """
         print("Você quer finalizar a votação?")
         print("0 - Continuar votação")
         print("1 - Finalizar votação")
@@ -51,6 +93,10 @@ class View:
         return self.menu()
 
     def voter_error(self, error):
+        """
+        Show user that he/she cannot vote
+        or entered an invalid registration number
+        """
         if error == 2:
             print()
             print("Eleitor não pode votar.")
@@ -67,15 +113,28 @@ class View:
             print()
 
     def get_voter_candidate(self, candidate_type):
+        """
+        Get candidate choice from user
+        Returns:
+            (int): number of the candidate
+        """
         print("{}: ".format(candidate_type))
         return int(input())
 
     def show_candidate_chosen(self, candidate_chosen):
+        """
+        Show the name and political party of the chosen candidate
+        """
         print()
         print("{}\n{}"
         .format(candidate_chosen['name'], candidate_chosen['political_party']))
 
     def get_voter_candidate_option(self):
+        """
+        Get confirmation of user from entered candidate
+        Returns:
+            (str): Choice entered by user
+        """
         print()
         print("Digite VERDE para CONFIRMAR este voto")
         print("Digite LARANJA para REINICIAR este voto")
@@ -83,12 +142,20 @@ class View:
         return input().lower()
 
     def show_voter_has_finished(self):
+        """
+        Show final message that voting system has finished
+        """
         print()
         print("FIM")
         print()
 
     def end(self, result):
-        if result == True:
+        """
+        Show user if system has finished correctly
+        Arguments:
+            result (bool): result of shutting down the voting process system
+        """
+        if result is True:
             print()
             print("### Sistema de votação finalizado com sucesso ###")
         else:
@@ -96,13 +163,21 @@ class View:
             print("### Sistema de votação finalizado com erro ###")
 
     def show_election_results(self, running_candidate, election_results, valid_votes):
+        """
+        Show the result of voting process. Show elected candidates
+        and vote count proportion
+        Arguments:
+            running_candidate (str): position that candidate is running for
+            election_results (list): list of candidate name and their vote count
+            valid_votes (int): quantity of valid votes, i.e., total votes count minus null votes
+        """
         print()
         print("### Resultados para candidato a {} ###".format(running_candidate))
         print()
 
         for candidate, votes in election_results:
 
-            if (valid_votes == 0):
+            if valid_votes == 0:
                 print("> {}, \t{} votos, \t{:.2%} dos votos"
                 .format(candidate, votes, votes)
                 )
