17e17e57081b32ecf8318742c21b10c228a0d39b
misaelrezende <38136358+misaelrezende@users.noreply.github.com>
2022-10-30 14:47:17 -0300

    Adiciona funcao de remocao de switch

--- src/server.c
+++ src/server.c
@@ -127,6 +127,24 @@ void ler_dados_de_switches(Rack *racks, int rack_da_operacao, int *switches_para
         informa_erro_e_termina_programa("error switch doesn't exist");
 
 }
+
+void remover_switch(Rack *racks, int rack_da_operacao, int *switches_para_operar){
+    // Rack vazio, não há nenhum switch a ser removido
+    if(racks[ rack_da_operacao - 1].quantidade_switches_alocados == 0)
+        informa_erro_e_termina_programa("error switch doesn't exist");
+    else if(racks[ rack_da_operacao - 1 ].switchs[ switches_para_operar[0] ].id_switch == switches_para_operar[0]){
+        // Remove switch
+        racks[ rack_da_operacao - 1 ].switchs[ switches_para_operar[0] ].id_switch = -1;
+        // Diminui por 1 o número de switches alocados
+        racks[ rack_da_operacao - 1].quantidade_switches_alocados -= 1;
+
+        // TODO: Falta enviar o nome do switch e do rack
+        informa_erro_e_termina_programa("switch <switch_id> removed from <rack_id>");
+    }else{
+        informa_erro_e_termina_programa("error switch doesn't exist");
+    }
+}
+
 // Processa o comando recebido
 void processar_comando(char *mensagem, Rack *racks){
     char *palavra; palavra = strtok(mensagem, " ");
@@ -188,6 +206,47 @@ void processar_comando(char *mensagem, Rack *racks){
 
     }else if(strcmp(palavra, "rm") == 0){
         printf("comando digitado: %s\n", palavra);
+
+        palavra = strtok(NULL, " ");
+        printf("palavra atual: %s\n", palavra);
+        if(strcmp(palavra, "sw") != 0){
+            // TODO: Mensagem desconhecida, ENCERRAR CONEXÃO
+            printf(">> mensagem [%s] desconhecida\nENCERRAR CONEXÃO\n", palavra); // tratamento EXTRA
+        }
+
+        while(palavra != NULL){
+            palavra = strtok(NULL, " ");
+            printf("palavra atual: %s\n", palavra);
+
+            if(palavra == NULL)
+                break;
+            if(strcmp(palavra, "in") == 0){
+                palavra = strtok(NULL, " ");
+                rack_da_operacao = atoi(palavra);
+                if(rack_da_operacao < 1 || rack_da_operacao > MAX_RACK){
+                    informa_erro_e_termina_programa("error rack doesn't exist\n"); // tratamento EXTRA
+                    break; // pode ser desnecessário
+                }
+            }
+            else{
+                int switch_id = atoi(palavra);
+                if(switch_id < 1 || switch_id > 4){ // tipo inválido de switch
+                    informa_erro_e_termina_programa("error switch type unknown");
+                    break; // pode ser desnecessário
+                }
+                switches_para_operar[contador_switches] = switch_id;
+                contador_switches += 1;
+            }
+
+        }
+        // NOTE: PRINTS EXTRAS
+        printf("contado_switches: %d | ", contador_switches);
+        for(int i = 0; i < 3; i++)
+            printf("%d ", switches_para_operar[i]);
+        printf(" | rack_da_operacao: %d\n", rack_da_operacao);
+
+        remover_switch(racks, rack_da_operacao, switches_para_operar);
+
     }else if(strcmp(palavra, "get") == 0){
         printf("comando digitado: %s\n", palavra);
 
