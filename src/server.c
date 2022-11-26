b3a639e130b565633ee3f61601febbbc76d33e82
misaelrezende <38136358+misaelrezende@users.noreply.github.com>
2022-11-26 15:18:51 -0300

    Corrige inicializacao padrao de equipamentos
    Remove atributo desnecessario de equipamento
    
    Adiciona remocao de equipamento

--- src/server.c
+++ src/server.c
@@ -21,14 +21,12 @@ void informa_erro_e_termina_programa(char *mensagem){
 }
 
 
-int contador_clientes = 0;
 // static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 // static pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 
 typedef struct{
 	int id;
 	float temperatura; // Entre 0 e 10, com duas casas decimais
-	int indice;
 	int socket_id;
 	struct sockaddr_in endereco_equipamento;
 	unsigned int tamanho;
@@ -38,12 +36,11 @@ Equipamento vetor_de_equipamentos[MAXCONNECTED];
 pthread_t vetor_de_threads[MAXCONNECTED];
 
 // Inicializa os equipamentos com valores padrão
-// Inicializa id, indice e socket_id com valor '-1'
+// Inicializa id e socket_id com valor '-1'
 void inicializar_equipamentos(Equipamento *equipamentos){
 	for(int i = 0; i < MAXCONNECTED; i++){
-		equipamentos->id = -1;
-		equipamentos->indice = -1;
-		equipamentos->socket_id = -1;
+		equipamentos[i].id = -1;
+		equipamentos[i].socket_id = -1;
 	}
 }
 
@@ -91,11 +88,30 @@ void verificar_erro_envio_de_mensagem(ssize_t numero_bytes_enviados, int tamanho
 		informa_erro_e_termina_programa("Falha no send().\nEnviado numero inesperado de bytes.\n");
 }
 
-char* processar_comando(char *mensagem){
+// Encontra uma posição livre no vetor de equipamentos
+int encontrar_posicao_livre(Equipamento *equipamentos){
+	int posicao;
+	for(int i = 0; i < MAXCONNECTED; i++){
+		if(equipamentos[i].id == -1){
+			posicao = i;
+			break;
+		}
+	}
+	return posicao;
+}
+
+// Remove equipamento do vetor de equipamentos
+void remover_equipamento(Equipamento *equipamento_atual){
+	equipamento_atual->id = -1;
+	equipamento_atual->socket_id = -1;
+}
+
+char* processar_comando(char *mensagem, Equipamento *equipamento_atual){
 	if(DEBUG == true)
 		printf("Processando comando: %s", mensagem);
 
 	if(strcmp(mensagem, "close connection\n") == 0){
+		remover_equipamento(equipamento_atual);
 		return "close connection";
 	}else if(strcmp(mensagem, "list equipment\n") == 0){
 		return "list equipment";
@@ -106,20 +122,19 @@ char* processar_comando(char *mensagem){
 
 void* comunicar(void* equipamento){
 	Equipamento* equipamento_atual = (Equipamento*) equipamento;
-	int indice = equipamento_atual->indice;
+	int id = equipamento_atual->id;
 	int socket_do_cliente = equipamento_atual->socket_id;
 	char *mensagem_para_retornar = NULL;
 	char mensagem[MAX_SIZE];
 
 	// Envia número de id a equipamento recentemente conectado
-	char msg[10] = "New ID: ", id[2];
-	sprintf(id, "%d", indice);  // converte id (int to char)
-	strcat(msg, id);
+	char msg[10] = "New ID: ", id_char[2];
+	sprintf(id_char, "%d", id);  // converte id (int to char)
+	strcat(msg, id_char);
 
 	ssize_t num_bytes_enviados = send(socket_do_cliente, msg, strlen(msg), 0);
 	verificar_erro_envio_de_mensagem(num_bytes_enviados, strlen(msg));
 
-	// printf("Equipamento %d conectado.\n", indice);
 
 	while(true){
 		ssize_t num_bytes_recebidos = recv(socket_do_cliente, mensagem, MAX_SIZE, 0);
@@ -128,9 +143,11 @@ void* comunicar(void* equipamento){
             informa_erro_e_termina_programa("Falha no recv() ao receber mensagem do equipamento.\n");
         else if(num_bytes_recebidos == 0){
             if(DEBUG == true)
-                printf("Conexao encerrada pelo equipamento %d.\n", indice);
+                printf("Conexao encerrada pelo equipamento %d.\n", id);
 
-			printf("Equipment %d removed\n", indice);
+			// Se equipamento encerrar abrutamente, remova equipamento
+			remover_equipamento(equipamento_atual);
+			printf("Equipment %d removed\n", id);
             close(socket_do_cliente);
             return NULL;
         }
@@ -141,7 +158,7 @@ void* comunicar(void* equipamento){
 
 		char resposta_para_cliente[MAX_SIZE]; // TODO: resposta para o equipamento?
 
-		mensagem_para_retornar = processar_comando(mensagem);
+		mensagem_para_retornar = processar_comando(mensagem, equipamento_atual);
 		if(mensagem_para_retornar == NULL){
 			printf("Mensagem desconhecida enviada por equipamento.\nA conexao com cliente sera encerrada.\n");
 			close(socket_do_cliente);
@@ -150,11 +167,12 @@ void* comunicar(void* equipamento){
 
 		if(strcmp(mensagem_para_retornar, "close connection") == 0){
 			if(DEBUG == true)
-				printf("Comando 'close connection' recebido.\nA conexao com o equipamento sera encerrada.\n");
+				printf("Comando 'close connection' recebido.\n");
 
+			printf("Equipment %d removed\n", id);
 			strcpy(resposta_para_cliente, "Sucess");
 
-			// TODO: Enviar mensagem também a outros equipamentos conectados
+			// TODO: Enviar mensagem também a outros equipamentos conectados (2.b.1)
 
 			ssize_t num_bytes_enviados = send(socket_do_cliente, resposta_para_cliente, strlen(resposta_para_cliente), 0);
 			verificar_erro_envio_de_mensagem(num_bytes_enviados, strlen(resposta_para_cliente));
@@ -186,10 +204,11 @@ int main(int argc, char* argv[]){
 
 	int socket_do_servidor = criar_conexao_tcp();
 	abrir_conexao_tcp(socket_do_servidor, numero_de_porta);
-	int socket_do_cliente;
+	int socket_do_cliente, contador_clientes = 0;
 
 	// Inicializa os equipamentos com valores padrão
 	inicializar_equipamentos(vetor_de_equipamentos);
+	int posicao_valida = 0; // próxima posicao válida do vetor
 
 	// TODO: Como limitar o número de equipamentos conectados?
 	while(true){
@@ -198,25 +217,25 @@ int main(int argc, char* argv[]){
 		else if(contador_clientes <= 9){
 			// Espera conexão de equipamento
 			socket_do_cliente = accept(socket_do_servidor,
-				(struct sockaddr*) &vetor_de_equipamentos[contador_clientes].endereco_equipamento,
-				&vetor_de_equipamentos[contador_clientes].tamanho);
+				(struct sockaddr*) &vetor_de_equipamentos[posicao_valida].endereco_equipamento,
+				&vetor_de_equipamentos[posicao_valida].tamanho);
 			if(socket_do_cliente < 0)
 				informa_erro_e_termina_programa("Falha no accept().\nEquipamento nao conectado\n");
 
-			vetor_de_equipamentos[contador_clientes].socket_id = socket_do_cliente;
-			vetor_de_equipamentos[contador_clientes].indice = contador_clientes;
+			vetor_de_equipamentos[posicao_valida].socket_id = socket_do_cliente;
+			vetor_de_equipamentos[posicao_valida].id = posicao_valida;
 
-			printf("Equipment %d added\n", contador_clientes);
-			// TODO: BROADCAST que equipamento foi adicionado (2.b.1)
+			printf("Equipment %d added\n", posicao_valida);
 			// TODO: Enviar a este equipamento a lista de equip. conectados na rede
 
-			int valor_de_retorno = pthread_create(&vetor_de_threads[contador_clientes], NULL, comunicar,
-				(void *) &vetor_de_equipamentos[contador_clientes]);
+			int valor_de_retorno = pthread_create(&vetor_de_threads[posicao_valida], NULL, comunicar,
+				(void *) &vetor_de_equipamentos[posicao_valida]);
 			if(valor_de_retorno != 0){
-				printf("Thread %lu: ", (unsigned long int) vetor_de_threads[contador_clientes]);
+				printf("Thread %lu: ", (unsigned long int) vetor_de_threads[posicao_valida]);
 				informa_erro_e_termina_programa("Falha no pthread_create().\n");
 			}
 
+			posicao_valida = encontrar_posicao_livre(vetor_de_equipamentos);
 			contador_clientes++;
 		}
  
